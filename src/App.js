be84a08c63d5b4b974f8e04d44fa8fd9e19e4818
Ramsey <seamusapple@163.com>
2022-11-13 16:57:39 +0800

    finish form page

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import Sidebar from "./scenes/global/Sidebar";
 import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
+import Form from "./scenes/form";
 // import Bar from "./scenes/bar";
-// import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
@@ -31,8 +31,8 @@ function App() {
               <Route path="/team" element={<Team></Team>}></Route>
               <Route path="/contacts" element={<Contacts></Contacts>}></Route>
               <Route path="/invoices" element={<Invoices></Invoices>}></Route>
+              <Route path="/form" element={<Form></Form>}></Route>
               {/* <Route path="/bar" element={<Bar></Bar>}></Route> */}
-              {/* <Route path="/form" element={<Form></Form>}></Route> */}
               {/* <Route path="/line" element={<Line></Line>}></Route> */}
               {/* <Route path="/pie" element={<Pie></Pie>}></Route> */}
               {/* <Route path="/faq" element={<FAQ></FAQ>}></Route> */}
